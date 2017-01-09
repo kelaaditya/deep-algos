35e7cfe776af53cbcc48b5113a9d688edf1e89f3
Aditya Kela <kelaaditya@gmail.com>
2017-01-09 20:18:26 +0100

    Indentation error fixed

--- Exercise_3/main.py
+++ Exercise_3/main.py
@@ -24,22 +24,22 @@ if __name__ == "__main__":
 	training_loss_05, test_accuracy_05, _, _ = cnn_orl(trainX, trainY, testX, testY, epochs, batch_size, 0.05)
 
 
-        plt.title('Training Loss vs Batch')
+	plt.title('Training Loss vs Batch')
 	plt.plot(np.arange(len(training_loss_01)), training_loss_01, label='eta = 0.01')
 	plt.plot(np.arange(len(training_loss_02)), training_loss_02, label='eta = 0.02')
 	plt.plot(np.arange(len(training_loss_03)), training_loss_03, label='eta = 0.03')
 	plt.plot(np.arange(len(training_loss_04)), training_loss_04, label='eta = 0.04')
 	plt.plot(np.arange(len(training_loss_05)), training_loss_05, label='eta = 0.05')
-        plt.legend()
+	plt.legend()
 	plt.show()
 
-        plt.title('Test Accuracy vs Batch')
+	plt.title('Test Accuracy vs Batch')
 	plt.plot(np.arange(len(test_accuracy_01)), test_accuracy_01, label='eta = 0.01')
 	plt.plot(np.arange(len(test_accuracy_02)), test_accuracy_02, label='eta = 0.02')
 	plt.plot(np.arange(len(test_accuracy_03)), test_accuracy_03, label='eta = 0.03')
 	plt.plot(np.arange(len(test_accuracy_04)), test_accuracy_04, label='eta = 0.04')
 	plt.plot(np.arange(len(test_accuracy_05)), test_accuracy_05, label='eta = 0.05')
-        plt.legend()
+	plt.legend()
 	plt.show()
 
 	#Visualization of the first CNN layer
