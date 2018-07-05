b0042c56734cb04040a4671b127cb4f94dbf6891
Aditya Kela <kelaaditya@gmail.com>
2018-07-05 18:19:04 +0530

    Combine batchify + load_mnist in main

--- MLP/mlp_mnist.py
+++ MLP/mlp_mnist.py
@@ -1,8 +1,84 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from load_mnist import *
-from batchify import *
+
+
+    ###########################
+    #         batchify        #
+    ###########################
+
+def batchify(X, y, batch_size):
+    for i in range(0, len(X)-batch_size+1, batch_size):
+        yield(X[i: i+batch_size], y[i: i+batch_size])
+
+
+
+    ###########################
+    #       load MNIST        #
+    ###########################
+
+"""
+Created on Tue Oct 25 11:40:10 2016
+
+Partly taken from https://github.com/tqchen/ML-SGHMC
+"""
+
+import os, struct
+from array import array as pyarray
+from numpy import  array, zeros
+import numpy as np
+
+def load_mnist(dataset="training", digits=np.arange(10), path="."):
+    """
+    Loads MNIST files into 3D numpy arrays 
+    Adapted from: http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
+    """
+
+    if dataset == "training":
+        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+    elif dataset == "testing":
+        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+    else:
+        raise ValueError("dataset must be 'testing' or 'training'")
+
+    flbl = open(fname_lbl, 'rb')
+    magic_nr, size = struct.unpack(">II", flbl.read(8))
+    lbl = pyarray("b", flbl.read())
+    flbl.close()
+
+    fimg = open(fname_img, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = pyarray("B", fimg.read())
+    fimg.close()
+
+    ind = [ k for k in range(size) if lbl[k] in digits ]
+    N = len(ind)
+
+    images = zeros((N, rows, cols) )
+    labels = zeros((N ) )
+    for i in range(len(ind)):
+        images[i] = array(img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
+        labels[i] = lbl[ind[i]]
+
+    return images, labels
+
+
+def load_data():
+    X_train, y_train = load_mnist('training')
+    X_test, y_test = load_mnist('testing')
+    X_train = np.reshape(X_train, (len(X_train), 1, 28, 28))
+    y_train = y_train.astype('int32')
+    X_test = np.reshape(X_test, (len(X_test), 1, 28, 28))
+    y_test = y_test.astype('int32')
+    return X_train/np.float32(256), y_train, X_test/np.float32(256), y_test
+
+
+
+    ###########################
+    #      MLP for MNIST      #
+    ###########################
 
 def mlp(batch_size, epochs, learning_rate=0.01):
     X = tf.placeholder(tf.float32, shape=(batch_size, 784), name='X')
