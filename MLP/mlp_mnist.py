6ccbd90d1854704cabf05e36fd49567ac59e3bfe
Aditya Kela <kelaaditya@gmail.com>
2018-07-06 11:06:36 +0530

    Add estimator APIs in mlp_mnist.py

--- MLP/mlp_mnist.py
+++ MLP/mlp_mnist.py
@@ -17,62 +17,13 @@ def batchify(X, y, batch_size):
     #       load MNIST        #
     ###########################
 
-"""
-Created on Tue Oct 25 11:40:10 2016
-
-Partly taken from https://github.com/tqchen/ML-SGHMC
-"""
-
-import os, struct
-from array import array as pyarray
-from numpy import  array, zeros
-import numpy as np
-
-def load_mnist(dataset="training", digits=np.arange(10), path="."):
-    """
-    Loads MNIST files into 3D numpy arrays 
-    Adapted from: http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
-    """
-
-    if dataset == "training":
-        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
-    elif dataset == "testing":
-        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
-    else:
-        raise ValueError("dataset must be 'testing' or 'training'")
-
-    flbl = open(fname_lbl, 'rb')
-    magic_nr, size = struct.unpack(">II", flbl.read(8))
-    lbl = pyarray("b", flbl.read())
-    flbl.close()
-
-    fimg = open(fname_img, 'rb')
-    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
-    img = pyarray("B", fimg.read())
-    fimg.close()
-
-    ind = [ k for k in range(size) if lbl[k] in digits ]
-    N = len(ind)
-
-    images = zeros((N, rows, cols) )
-    labels = zeros((N ) )
-    for i in range(len(ind)):
-        images[i] = array(img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
-        labels[i] = lbl[ind[i]]
-
-    return images, labels
-
-
-def load_data():
-    X_train, y_train = load_mnist('training')
-    X_test, y_test = load_mnist('testing')
-    X_train = np.reshape(X_train, (len(X_train), 1, 28, 28))
-    y_train = y_train.astype('int32')
-    X_test = np.reshape(X_test, (len(X_test), 1, 28, 28))
-    y_test = y_test.astype('int32')
-    return X_train/np.float32(256), y_train, X_test/np.float32(256), y_test
+def get_mnist_data():
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_data = mnist.train.images
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = mnist.test.images
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+    return(train_data, train_labels, eval_data, eval_labels)
 
 
 
@@ -123,14 +74,60 @@ def mlp(batch_size, epochs, learning_rate=0.01):
 
     return(training_loss_list)
 
-if __name__ == "__main__":
-    from tensorflow.examples.tutorials.mnist import input_data
-    mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-    
-    training_loss = mlp(batch_size=50, 
-                        epochs=100,
-                        learning_rate=0.01)
 
-    plt.plot(np.arange(len(training_loss)), training_loss)
-    plt.show()
+
+    ###########################
+    #   MLP (HighLevelAPIs)   #
+    ###########################
+
+def mlp_model_fn(features, labels, mode):
+    input_layer = tf.reshape(features["x"], [-1, 784])
     
+    hidden1 = tf.layers.dense(inputs=input_layer, units=1024, activation=tf.nn.relu)
+    
+    hidden2 = tf.layers.dense(inputs=hidden1, units=1024, activation=tf.nn.relu)
+    
+    logits = tf.layers.dense(inputs=hidden2, units=10)
+    
+    predictions = {
+        "classes": tf.argmax(input=logits, axis=1),
+        "probabilities": tf.nn.softmax(logits, name="softmax")
+    }
+    
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        spec = tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+    else:
+        loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+        
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+            train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+            spec = tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+        if mode == tf.estimator.ModeKeys.EVAL:
+            eval_metric_ops = {
+                "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
+            }
+            spec = tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    return(spec)
+
+
+
+if __name__ == "__main__":
+    x_train, y_train, x_eval, y_eval = get_mnist_data()
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": x_train},
+                                                    y=y_train,
+                                                    num_epochs=2,
+                                                    batch_size=100,
+                                                    shuffle=True)
+
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": x_eval},
+                                                   y=y_eval,
+                                                   num_epochs=1,
+                                                   batch_size=1,
+                                                   shuffle=False)
+    
+    mnist_classifier = tf.estimator.Estimator(model_fn=mlp_model_fn, model_dir="./checkpoints/mnist_mlp")
+    mnist_classifier.train(input_fn=train_input_fn, steps=20000)
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
