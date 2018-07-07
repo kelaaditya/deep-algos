e52c37dc6a7f4a397b69e5541937ee6a713c3eb6
Aditya Kela <kelaaditya@gmail.com>
2018-07-07 17:32:56 +0530

    Combine helper code into orl.py

--- convolutional_neural_net/batchify.py
+++
@@ -1,3 +0,0 @@
-def batchify(X, y, batch_size):
-    for i in range(0, len(X)-batch_size+1, batch_size):
-        yield(X[i: i+batch_size], y[i: i+batch_size])
--- convolutional_neural_net/orl.py
+++ convolutional_neural_net/orl.py
@@ -2,11 +2,21 @@ import theano
 import theano.tensor as T
 import lasagne
 import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
-from batchify import *
-from load_ORL_faces import *
+
+# load data
+data = np.load('ORL_faces.npz')
+trainX = np.reshape(data['trainX'], (240, 1, 92, 112))/256
+trainY = data['trainY']
+testX = np.reshape(data['testX'], (160, 1, 92, 112))/256
+testY = data['testY']
+
+
+def batchify(X, y, batch_size):
+    for i in range(0, len(X)-batch_size+1, batch_size):
+        yield(X[i: i+batch_size], y[i: i+batch_size])
 
 
 def cnn_orl(X_tr, y_tr, X_t, y_t, epochs, batch_size, learning_rate=0.01):
@@ -89,7 +99,49 @@ def cnn_orl(X_tr, y_tr, X_t, y_t, epochs, batch_size, learning_rate=0.01):
     return(training_loss_list, test_accuracy_list, W_1, b_1)
 
 
+
 if __name__ == "__main__":
-    training_loss, test_accuracy, _, _ = cnn_orl(trainX, trainY, testX, testY, 1, 10, 0.01)
-    print(np.array(training_loss))
-    print(test_accuracy)
+	epochs = 100
+	batch_size  = 10
+
+	'''
+	Training Loss and Test Accuracy calculated for different learning rates: {0.01, 0.02, 0.03, 0.04, 0.05}
+	'''
+	training_loss_01, test_accuracy_01, W, b = cnn_orl(trainX, trainY, testX, testY, epochs, batch_size, 0.01)
+	training_loss_02, test_accuracy_02, _, _ = cnn_orl(trainX, trainY, testX, testY, epochs, batch_size, 0.02)
+	training_loss_03, test_accuracy_03, _, _ = cnn_orl(trainX, trainY, testX, testY, epochs, batch_size, 0.03)
+	training_loss_04, test_accuracy_04, _, _ = cnn_orl(trainX, trainY, testX, testY, epochs, batch_size, 0.04)
+	training_loss_05, test_accuracy_05, _, _ = cnn_orl(trainX, trainY, testX, testY, epochs, batch_size, 0.05)
+
+
+	plt.title('Training Loss vs Batch')
+	plt.plot(np.arange(len(training_loss_01)), training_loss_01, label='eta = 0.01')
+	plt.plot(np.arange(len(training_loss_02)), training_loss_02, label='eta = 0.02')
+	plt.plot(np.arange(len(training_loss_03)), training_loss_03, label='eta = 0.03')
+	plt.plot(np.arange(len(training_loss_04)), training_loss_04, label='eta = 0.04')
+	plt.plot(np.arange(len(training_loss_05)), training_loss_05, label='eta = 0.05')
+	plt.legend()
+	plt.show()
+
+	plt.title('Test Accuracy vs Batch')
+	plt.plot(np.arange(len(test_accuracy_01)), test_accuracy_01, label='eta = 0.01')
+	plt.plot(np.arange(len(test_accuracy_02)), test_accuracy_02, label='eta = 0.02')
+	plt.plot(np.arange(len(test_accuracy_03)), test_accuracy_03, label='eta = 0.03')
+	plt.plot(np.arange(len(test_accuracy_04)), test_accuracy_04, label='eta = 0.04')
+	plt.plot(np.arange(len(test_accuracy_05)), test_accuracy_05, label='eta = 0.05')
+	plt.legend()
+	plt.show()
+
+	#Visualization of the first CNN layer
+	#Weights and biases added together
+	#Each filter (30) displayed in the grid
+	first_layer_weights = [weights[0]+bias for weights, bias in zip(W.get_value(), b.get_value())]
+	gs = gridspec.GridSpec(10, 10)
+	for i in range(len(first_layer_weights)):
+		ax = plt.subplot(gs[i])
+		ax.grid()
+		ax.set_xticks([])
+		ax.set_yticks([])
+		ax.imshow(first_layer_weights[i], cmap='gray')
+	plt.show()
+		
