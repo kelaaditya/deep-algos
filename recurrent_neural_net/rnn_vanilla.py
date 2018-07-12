2374bc1c3470f291aa520a24a16b1febc8aa3c5d
Aditya Kela <kelaaditya@gmail.com>
2018-07-13 02:04:05 +0530

    Add train function for echo network

--- recurrent_neural_net/rnn_vanilla.py
+++ recurrent_neural_net/rnn_vanilla.py
@@ -1,13 +1,15 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 num_classes = 2
-length_step = 25
-hidden_size = 100
-length_train = 10000
+length_step = 10
+length_train = 100000
 length_echo = 5
-batch_size = 1
-state_size = 100
+batch_size = 10
+state_size = 10
+learning_rate = 0.001
+
 
 def generate_data(length_train, length_echo, batch_size, length_step):
     assert length_train % (batch_size * length_step) == 0
@@ -21,35 +23,67 @@ def generate_data(length_train, length_echo, batch_size, length_step):
 
     return(x_train, y_train)
 
-x = tf.placeholder(dtype=tf.int64, shape=[batch_size, length_step])
-y = tf.placeholder(dtype=tf.int64, shape=[batch_size, length_step])
-init_state = tf.placeholder(dtype=tf.float32, shape=[batch_size, state_size])
 
-x_one_hot = tf.one_hot(x, num_classes)
-x_one_hot_stack = tf.unstack(x_one_hot, axis=1)
-
-def RNN_cell(rnn_input, state):
+def RNN_cell(state, input_vector):
     with tf.variable_scope('RNN_cell', reuse=tf.AUTO_REUSE):
-        W = tf.get_variable('W', shape=[state_size + num_classes, state_size], initializer=tf.random_normal_initializer())
-        b = tf.get_variable('b', shape=[state_size], initializer=tf.constant_initializer(0.0))
-    concatenation = tf.concat([rnn_input, state], 1)
-    output = tf.add(tf.matmul(concatenation, W), b)
-    output_nonlinear = tf.tanh(output)
-    return(output_nonlinear)
+        W = tf.get_variable('W', [num_classes + state_size, state_size], initializer=tf.random_normal_initializer())
+        b = tf.get_variable('b', [state_size], initializer=tf.zeros_initializer())
+    concatenation = tf.concat([input_vector, state], axis=1)
+    nonlinear_output = tf.tanh(tf.add(tf.matmul(concatenation, W), b))
+    return(nonlinear_output)
 
-def RNN_cell_outputs(init_state,
+def RNN_cell_outputs(initial_state, list_of_inputs):
     cell_outputs = []
-    state = init_state
-    for input_vector in x_one_hot_stack:
-        state = RNN_cell(input_vector, state)
+    state = initial_state
+    for input_vector in list_of_inputs:
+        state = RNN_cell(state, input_vector)
         cell_outputs.append(state)
+    return(cell_outputs)
 
-with tf.variable_scope('softmax'):
-    W = tf.get_variable('W', [state_size, num_classes], initializer=tf.random_normal_initializer())
-    b = tf.get_variable('b', [num_classes], initializer=tf.constant_initializer(0.0))
-logits = [tf.add(tf.matmul(cell_output, W), b) for cell_output in cell_outputs]
-predictions = [tf.nn.softmax(logit) for logit in logits]
 
-x_train, y_train = generate_data(length_train, length_echo, batch_size)
-x_train = np.reshape(x_train, newshape=(length_train//length_step, batch_size, length_step))
-x_train
+def RNN_logits(initial_state, list_of_inputs):
+    with tf.variable_scope('softmax', reuse=tf.AUTO_REUSE):
+        W = tf.get_variable('W', [state_size, num_classes], initializer=tf.random_normal_initializer())
+        b = tf.get_variable('b', [num_classes], initializer=tf.zeros_initializer())
+                                    
+    cell_outputs = RNN_cell_outputs(initial_state, list_of_inputs)
+                                            
+    logits = [tf.add(tf.matmul(cell_output, W), b) for cell_output in cell_outputs]
+                                                        
+    return(logits)
+
+
+def RNN_train(num_epochs):
+    x = tf.placeholder(dtype=tf.int32, shape=[batch_size, length_step])
+    y = tf.placeholder(dtype=tf.int32, shape=[batch_size, length_step])
+    init_state = tf.placeholder(dtype=tf.float32, shape=[batch_size, state_size])
+
+    x_one_hot = tf.one_hot(x, depth=2)
+    x_one_hot_stack = tf.unstack(x_one_hot, axis=1)
+
+    logits = RNN_logits(init_state, x_one_hot_stack)
+    
+    y_unstack = tf.unstack(y, axis=1)
+    
+    loss = tf.reduce_mean([tf.nn.sparse_softmax_cross_entropy_with_logits(labels=label, logits=logit) for logit, label in zip(logits, y_unstack)])
+    
+    train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+    
+    init = tf.global_variables_initializer()
+    
+    with tf.Session() as sess:
+        sess.run(init)
+        loss_list = []
+        for epoch in range(num_epochs):
+            print("epoch #: ", epoch)
+            x_train, y_train = generate_data(length_train, length_echo, batch_size, length_step)
+            training_state = np.zeros((batch_size, state_size))
+            for x_data, y_data in zip(x_train, y_train):
+                training_loss, _ = sess.run([loss, train_op], feed_dict={x: x_data, y: y_data, init_state: training_state})
+                loss_list.append(training_loss)
+    return(loss_list)
+
+if __name__=="__main__":
+    loss_list = RNN_train(num_epochs=10)
+    plt.plot(loss_list)
+    plt.show()
