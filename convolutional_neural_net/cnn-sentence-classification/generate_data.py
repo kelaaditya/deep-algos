b517bf63aaa03e780d577dc50875fdad2ec2dce7
Aditya Kela <kelaaditya@gmail.com>
2018-07-26 21:35:35 +0530

    add generate_epoch. Changed generate_data to output indices with padding

--- convolutional_neural_net/cnn-sentence-classification/generate_data.py
+++ convolutional_neural_net/cnn-sentence-classification/generate_data.py
@@ -19,31 +19,55 @@ def clean(string):
     return(string)
 
 
-def get_data(positive_examples, negative_examples, batch_size, num_epoch):
+def get_data(positive_examples, negative_examples, batch_size):
     with open(positive_examples) as pos_file:
         pos_lines = pos_file.readlines()
         pos_x = [clean(pos_line.strip()) for pos_line in pos_lines]
     with open(negative_examples) as neg_file:
         neg_lines = neg_file.readlines()
         neg_x = [clean(neg_line.strip()) for neg_line in neg_lines]
-    x_data = np.array(pos_data + neg_data)
-                                                            
+    x_data = pos_x + neg_x
+    
     data_size = len(x_data)
 
     pos_y = [[0, 1]] * len(pos_x)
     neg_y = [[1, 0]] * len(neg_x)
     y_data = np.concatenate([pos_y, neg_y])
-                                                                            
+    
+    max_sentence_length = max([len(x.split(" ")) for x in x_data])
+    
+    words = set([item for x in x_data for item in x.split(" ")])
+    index_to_word = dict(enumerate(words))
+    word_to_index = dict(zip(index_to_word.values(), index_to_word.keys()))
+
+    # First, create a zero matrix of shape (len(x_data)) x max_sentence_length
+    # We pad each sentence with zeros so that all sentences have the same length
+    # We then convert each word of each sentence to it's index calculated in the
+    # 'word_to_index' dictionary.
+    # The padded zeros remain unchanged for each sentence
+    x_index = np.zeros(shape=(len(x_data), max_sentence_length), dtype=np.int32)
+    for i in range(len(x_index)): 
+        word_row = x_data[i].split(" ")
+        for j in range(len(word_row)):
+            x_index[i][j] = word_to_index[word_row[j]]
+
+    # permute the x_index rows and the y_data rows according to the 
+    # list of permutations defined by np.random.permutation
     permutations = np.random.permutation(data_size)
-    x_data = x_data[permutations]
+    x_index = x_index[permutations]
     y_data = y_data[permutations]
-                                                                                                
+    
+    # mould x_index and y_data into batches
     num_batches = data_size // batch_size
-    x_data = x_data[:num_batches * batch_size]
+    x_index = x_index[:num_batches * batch_size]
     y_data = y_data[:num_batches * batch_size]
-    x_data = np.reshape(x_data, newshape=(num_batches, batch_size))
+    x_index = np.reshape(x_index, newshape=(num_batches, batch_size, max_sentence_length))
     y_data = np.reshape(y_data, newshape=(num_batches, batch_size, 2))
-                    
-    for epoch in range(num_epoch):
-        for batch in range(num_batches):
+
+    return(x_index, y_data)
+
+
+def generate_epochs(x_data, y_data, num_epochs):
+    for epoch in range(num_epochs):
+        for batch in range(len(x_data)):
             yield(x_data[batch], y_data[batch])
