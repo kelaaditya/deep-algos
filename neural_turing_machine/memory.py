9c76bd6449fb022a9848eea8eaecd97833bb9514
Aditya Kela <kelaaditya@gmail.com>
2018-09-04 15:51:57 +0530

    added content addressing. add location addressing

--- neural_turing_machine/memory.py
+++ neural_turing_machine/memory.py
@@ -25,3 +25,37 @@ class Memory:
         self.batch_size = batch_size
 
 
+    def content_addressing(self, memory, read_and_write_key_vectors, read_and_write_strengths):
+        """content-based addressing from memory
+        
+        Parameters:
+        -----------
+        
+        memory: tf.Tensor 
+            shape: (batch_size, num_memory_vectors, size_memory_vector)
+            the memory matrix
+        read_and_write_key_vectors: tf.Tensor
+            shape: (batch_size, num_total_keys, size_memory_vector)
+            concatenated read and write key vectors
+        read_and_write_strengths: tf.Tensor
+            shape: (batch_size, num_total_keys)
+            tensor of read and write strengths
+            
+        Returns:
+        --------
+        tf.Tensor
+            shape: (batch_size, num_memory_vectors, num_total_keys)
+        """
+        normalized_memory = tf.nn.l2_normalize(memory, axis=2)
+        
+        transpose_read_and_write_key_vectors = tf.transpose(read_and_write_key_vectors, perm=[0, 2, 1])
+        normalized_read_and_write_key_vectors = tf.nn.l2_normalize(transpose_read_and_write_key_vectors, axis=1)
+        
+        conv_matrix = tf.matmul(normalized_memory, normalized_read_and_write_key_vectors)
+        
+        read_and_write_strengths = tf.expand_dims(read_and_write_strengths, 1)
+        
+        # multiply instead of matmul as each strength gets multiplied
+        # with each similarity vector to get out a num_total_key
+        # number of addresses
+        return tf.nn.softmax(conv_matrix * read_and_write_strengths, axis=1)
