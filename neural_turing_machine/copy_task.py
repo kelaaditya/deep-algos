4d58a531b9bc2e7389c4019b0ae65b7b2a2ce8a4
Aditya Kela <kelaaditya@gmail.com>
2018-09-14 20:30:44 +0530

    added argument parsing. add plotting function

--- neural_turing_machine/copy_task.py
+++ neural_turing_machine/copy_task.py
@@ -1,13 +1,9 @@
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import tensorflow as tf
 
-from feedforward_controller import FeedForwardController
-from lstm_controller import LSTMController
-from memory import Memory
 from neural import NTM
-
 
 
 def generate_data(batch_size, size_input_sequence, size_input):
@@ -34,14 +30,23 @@ def generate_data(batch_size, size_input_sequence, size_input):
     return input_sequential_data, target_output
 
 
-def train(model='lstm', # or 'feedforward'
-          learning_rate=0.001,
-          momentum=0.9,
-          decay=0.95,
-          iterations=100000,
-          save_location='./checkpoints/',
-          restore_training=False):
-    
+def train(model, # 'lstm' or 'feedforward'
+          learning_rate,
+          momentum,
+          decay,
+          iterations,
+          save_location,
+          restore_training,
+          size_input,
+          size_input_sequence,
+          size_output,
+          num_memory_vectors,
+          size_memory_vector,
+          num_read_heads,
+          num_write_heads,
+          size_conv_shift,
+          batch_size):
+
     tf.reset_default_graph()
     
     ntm = NTM(model,
@@ -93,19 +98,32 @@ def train(model='lstm', # or 'feedforward'
         for iteration in range(iterations):
             input_data, target_output_data = generate_data(batch_size, size_input_sequence, size_input)
             
-            current_loss, _, memory_tensor = sess.run([loss, train_op, ntm.memory_tensor], feed_dict={ntm.input_sequential_data: input_data, ntm.target_output: target_output_data})
+            current_loss, _, memory_tensor = sess.run([loss, train_op, ntm.memory_tensor],
+                                                      feed_dict={
+                                                          ntm.input_sequential_data: input_data,
+                                                          ntm.target_output: target_output_data
+                                                      })
             
             loss_list.append(current_loss)
             memory_tensor_list.append(memory_tensor)
             
             if iteration % 1000 == 0:
                 saver.save(sess, save_location + model, global_step=iteration)
-                
+
     return loss_list, memory_tensor_list
 
 
-def test(model='lstm', # or 'feedforward'
-         save_location='./checkpoints/'):
+def test(model, # or 'feedforward'
+         save_location,
+         size_input,
+         size_input_sequence,
+         size_output,
+         num_memory_vectors,
+         size_memory_vector,
+         num_read_heads,
+         num_write_heads,
+         size_conv_shift,
+         batch_size):
     
     tf.reset_default_graph()
     
@@ -128,31 +146,74 @@ def test(model='lstm', # or 'feedforward'
         saver.restore(sess, ckpt.model_checkpoint_path)
 
         input_data, target_output = generate_data(batch_size, size_input_sequence, size_input)
-        
         ntm_output, memory_tensor = ntm.generate_output(ntm.input_sequential_data)
-                
-        ntm_output, memory_tensor = sess.run([ntm_output, memory_tensor], feed_dict={ntm.input_sequential_data: input_data})
+        ntm_output, memory_tensor = sess.run([ntm_output, memory_tensor],
+                                             feed_dict={ntm.input_sequential_data: input_data})
         
-    return ntm_output, target_output, memory_tensor
+    return input_data, ntm_output, target_output, memory_tensor
+
+
+def main():
+    parser = argparse.ArgumentParser(description='NTM - Copy Task')
+    # mode: training vs testing
+    parser.add_argument('--mode',               type=str,   default='train',    help='training vs testing')
+
+    # model: LSTMController vs FeedForwardController
+    parser.add_argument('--model',              type=str,   default='lstm',     help='controller: LSTM vs Feedforward')
+
+    # training parameters
+    parser.add_argument('--iterations',         type=int,   default=100000,     help='total training iterations')
+    parser.add_argument('--restore_training',   type=str,   default=False,      help='restore training from checkpoint')
+    parser.add_argument('--learning_rate',      type=float, default=0.001)
+    parser.add_argument('--momentum',           type=float, default=0.9)
+    parser.add_argument('--decay',              type=float, default=0.95)
+    parser.add_argument('--save_location',      type=str,   default='./checkpoints/')
+
+    # model parameters
+    parser.add_argument('--size_input',         type=int,   default=10)
+    parser.add_argument('--size_input_sequence',type=int,   default=10)
+    parser.add_argument('--size_output',        type=int,   default=10)
+    parser.add_argument('--num_memory_vectors', type=int,   default=128)
+    parser.add_argument('--size_memory_vector', type=int,   default=64)
+    parser.add_argument('--num_read_heads',     type=int,   default=1)
+    parser.add_argument('--num_write_heads',    type=int,   default=1)
+    parser.add_argument('--size_conv_shift',    type=int,   default=1)
+    parser.add_argument('--batch_size',         type=int,   default=1)
+
+    args = parser.parse_args()
+
+    if args.mode == 'train':
+        loss_list, _ = train(model=args.model,
+                             learning_rate=args.learning_rate,
+                             momentum=args.momentum,
+                             decay=args.decay,
+                             iterations=args.iterations,
+                             save_location=args.save_location,
+                             restore_training=args.restore_training,
+                             size_input=args.size_input,
+                             size_input_sequence=args.size_input_sequence,
+                             size_output=args.size_output,
+                             num_memory_vectors=args.num_memory_vectors,
+                             size_memory_vector=args.size_memory_vector,
+                             num_read_heads=args.num_read_heads,
+                             num_write_heads=args.num_write_heads,
+                             size_conv_shift=args.size_conv_shift,
+                             batch_size=args.batch_size)
+
+    else:
+        input_data, ntm_output, target_output, memory_tensor = test(model=args.model,
+                                                                    save_location=args.save_location,
+                                                                    size_input=args.size_input,
+                                                                    size_input_sequence=args.size_input_sequence,
+                                                                    size_output=args.size_output,
+                                                                    num_memory_vectors=args.num_memory_vectors,
+                                                                    size_memory_vector=args.size_memory_vector,
+                                                                    num_read_heads=args.num_read_heads,
+                                                                    num_write_heads=args.num_write_heads,
+                                                                    size_conv_shift=args.size_conv_shift,
+                                                                    batch_size=args.batch_size)
 
 
 
 if __name__ == "__main__":
-
-    size_input = 10
-    size_input_sequence = 10
-    size_output = 10
-    num_memory_vectors = 128
-    size_memory_vector = 64
-    num_read_heads = 1
-    num_write_heads = 1
-    size_conv_shift = 1
-    batch_size = 1
-
-    loss_list, memory_tensor_list = train(iterations=100000, restore_training=False)
-
-    plt.plot(loss_list)
-    plt.xlabel('iterations')
-    plt.title('Training Loss')
-    plt.savefig('training_loss.png')
-    print(loss_list)
+    main()
