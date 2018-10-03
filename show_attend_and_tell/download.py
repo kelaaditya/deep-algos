f97ed23427221822bea1c332d5bb7c44aeed9bd5
Aditya Kela <kelaaditya@gmail.com>
2018-10-03 16:28:03 +0530

    Added unzip via zipfile

--- show_attend_and_tell/download.py
+++ show_attend_and_tell/download.py
@@ -2,6 +2,7 @@ import math
 import os
 import requests
 from tqdm import tqdm
+import zipfile
 
 VGG19 = 'http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat'
 
@@ -18,6 +19,10 @@ file_names = {
         COCO_ANNOTATE: 'annotations_trainval2014.zip'
         }
 
+
+    #####################
+    #    Downloading    #
+    #####################
 
 def download(URL):
     download_location = 'data/'
@@ -45,18 +50,53 @@ def download(URL):
                     if chunk:
                         data_file.write(chunk)
                         pbar.update(len(chunk))
+        print(f'{file_names[URL]} downloaded.')
 
-    return file_name
+    return file_name, file_size
+
+
+    #####################
+    #     Unzipping     #
+    #####################
+
+def unzip(URL):
+    file_location = 'data/'
+    file_name = os.path.join(file_location, file_names[URL])
+
+    with zipfile.ZipFile(file_name, 'r') as zf:
+        uncompressed_size = sum(file.file_size for file in zf.infolist())
+
+        with tqdm(total=uncompressed_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            for sub_file in zf.infolist():
+                zf.extract(sub_file, )
+                pbar.update(sub_file.file_size)
+
+    print(f'{file_names[URL]} has been extracted.')
+
+
+    ######################
+    #  Download + Unzip  #
+    ######################
+
+def download_and_unzip(URL):
+    file_name, file_size = download(URL)
+
+    if not os.path.exists(file_name) or not os.stat(file_name).st_size == file_size:
+        raise ValueError(f'{file_names[URL]} download was incomplete. Cannot proceed with unzip.')
+
+    unzip(URL)
 
 
 if __name__=="__main__":
     try:
         download(VGG19)
-        download(COCO_TRAIN)
-        download(COCO_VALIDATE)
-        download(COCO_ANNOTATE)
+        download_and_unzip(COCO_TRAIN)
+        download_and_unzip(COCO_VALIDATE)
+        download_and_unzip(COCO_ANNOTATE)
     except requests.exceptions.ConnectionError as ce:
         print(ce)
+    except ValueError as ve:
+        print(ve)
     except Exception as e:
         print(e)
 
